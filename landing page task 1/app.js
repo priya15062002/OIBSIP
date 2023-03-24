28a9c6ef2bc2664b9066ecec17351c3d7470b1ae
priya15062002 <109334108+priya15062002@users.noreply.github.com>
2023-03-24 02:27:52 -0700

    Add files via upload

---
+++ landing page task 1/app.js
@@ -0,0 +1,6 @@
+const hamburger=document.getElementById("hamburger")
+const navlist=document.getElementById("navlist")
+
+hamburger.addEventListener("click",()>={
+    navlist.classList.toggle("navlist.active")
+})
